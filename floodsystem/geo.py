7319d4f852b77e0ad5aab901786f8e8ceec55565
AlexNimmo25 <an729@cam.ac.uk>
2025-01-26 14:16:04 +0000

    part of task 1c completed - need to impelement

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,3 +10,33 @@ from .utils import sorted_by_key  # noqa
 
 def stations_by_distance():
     pass
+
+    
+
+import math
+def haversine( lat1, long1, lat2, long2):
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    
+    # Radius of Earth in kilometers
+    R = 6371
+    distance = R * c
+    return distance
+
+
+def stations_in_radius(stations, centre, r):
+    lat_centre, long_centre = centre
+    stations_in_radius = []
+    for station in stations:
+        lat_station, long_station = station.coord
+        distance = haversine(lat_centre, long_centre, lat_station, long_station)
+        if distance <= r:
+            
+            stations_in_radius.append(station)
+
+            
+    return stations_in_radius
+    
