50205c5fb0fcfe9a195b884cd7e342a92cfb2bbc
andrelovesfishing <am3331@cam.ac.uk>
2025-01-26 14:10:42 +0000

    Bulk of Task1B completed without selecting stations

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -3,10 +3,17 @@
 # SPDX-License-Identifier: MIT
 """This module contains a collection of functions related to
 geographical data.
-
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+from floodsystem.stationdata import build_station_list
 
-def stations_by_distance():
+def stations_by_distance(stations, p):
+    "Build and return a list of stations with their respective distances from co-ordinate p"
+    list_of_stations = []
+    for station in stations:
+        list_of_stations.append((station.name, haversine(station.coord, p)))
+    list_of_stations = sorted_by_key(list_of_stations, 1)
+    print(list_of_stations)
     pass
