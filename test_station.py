975e7c3d4c8e8a0402cd012578a5c17636e58903
andrelovesfishing <am3331@cam.ac.uk>
2025-01-26 19:57:09 +0000

    Added test less than

--- test_station.py
+++ test_station.py
@@ -26,7 +26,7 @@ def test_create_monitoring_station():
     assert s.river == river
     assert s.town == town
 
-def test_typical_range_consistent(station):
+def test_typical_range_consistent():
     # Create a consistent station
     s_id = "test-s-id"
     m_id = "test-m-id"
@@ -52,7 +52,7 @@ def test_typical_range_consistent(station):
     m_id = "test-m-id"
     label = "some station"
     coord = (-2.0, 4.0)
-    trange = ()
+    trange = None
     river = "River X"
     town = "My Town"
     station3 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
@@ -60,3 +60,29 @@ def test_typical_range_consistent(station):
     assert station1.typical_range_consistent() == True
     assert station2.typical_range_consistent() == False
     assert station3.typical_range_consistent() == False
+
+def test_less_than():
+    # Create a station with an alphabetically lower ranked name
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "adrian"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    station1 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    # Create a station with an alphabetically lower ranked name
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "jamal"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, -2.4)
+    river = "River X"
+    town = "My Town"
+    station2 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    assert station1 < station2
+
+test_typical_range_consistent()
+test_less_than()
