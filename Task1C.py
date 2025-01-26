3b922754f58b05d74a784c12e0339ce6252792b8
andrelovesfishing <am3331@cam.ac.uk>
2025-01-26 18:50:43 +0000

    Added correct formatting

--- Task1C.py
+++ Task1C.py
@@ -1,6 +1,11 @@
 from floodsystem.geo import stations_in_radius
 from floodsystem.stationdata import build_station_list
 
-stations = build_station_list()
-f = stations_in_radius(stations, (52.2053, 0.1218), 10)
-print(f)
+def run():
+    stations = build_station_list()
+    f = stations_in_radius(stations, (52.2053, 0.1218), 10)
+    print(f)
+
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
