7d233bc465cca3dd1092b27892991117b7c0d22b
AlexNimmo25 <an729@cam.ac.uk>
2025-01-26 18:44:09 +0000

    task 1e completed and implemented

---
+++ Task1E.py
@@ -0,0 +1,6 @@
+from floodsystem.geo import rivers_by_station_number
+from floodsystem.stationdata import build_station_list
+
+stations = build_station_list()
+x = rivers_by_station_number(stations, 9)
+print(x)
