b62553dd360b58a56e813265ba343d9c1917f6c0
andrelovesfishing <am3331@cam.ac.uk>
2025-02-20 20:55:01 +0000

    Completed task2B, with flood submodule and relative water function

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,2 +1,13 @@
+from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import update_water_levels
+
 def stations_level_over_threshold(stations, tol):
-    pass
+    update_water_levels(stations)
+    stations_over_threshold = []
+    for station in stations:
+        if station.relative_water_level() is not None and station.typical_range_consistent() is True:
+            if station.relative_water_level() > tol:
+                stations_over_threshold.append((station.name, station.relative_water_level()))
+    
+    stations_over_threshold = sorted(stations_over_threshold, key=lambda x: x[1], reverse=True)
+    return stations_over_threshold
