e8b0cf4cf5afe7aedd7306e85c1d338f8df740a4
AlexNimmo25 <an729@cam.ac.uk>
2025-02-20 22:51:28 +0000

    2E done

---
+++ Task2E.py
@@ -0,0 +1,51 @@
+from floodsystem.plot import plot_water_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.datafetcher import fetch_measure_levels
+import datetime
+
+def run():
+    stations = build_station_list()
+    top_five_stations = []
+    stations_list = stations_highest_rel_level(stations,5)
+    for item in stations_list:
+        top_five_stations.append(item[0])
+
+    print(top_five_stations)
+    # Build list of stations
+    
+   
+    # Station name to find
+    for station in top_five_stations:
+        station_name = station
+
+    # Find station
+        station_cam = None
+        for station in stations:
+         if station.name == station_name:
+            station_cam = station
+            break
+
+    # Check that station could be found. Return if not found.
+        if not station_cam:
+            print("Station {} could not be found".format(station_name))
+            return
+    
+    # Fetch and plot data for the past 10 days for each of these stations
+        dt = 10
+        dates, levels = fetch_measure_levels(
+            station_cam.measure_id, dt=datetime.timedelta(days=dt))
+        list_of_dates = []
+        list_of_levels = []
+        for date, level in zip(dates, levels):
+            list_of_dates.append(date.replace(tzinfo=None))
+            list_of_levels.append(level)
+    # Print level history
+    
+        plot_water_levels(station_cam, list_of_dates, list_of_levels)
+
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
+
+
---
+++ floodsystem/plot.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import MonitoringStation
+
+def plot_water_levels(station, dates, levels):
+    """Plot water levels for a station."""
+
+    v = station.typical_range[0]
+    h = station.typical_range[1]
+    # Plot
+    plt.plot(dates, levels)
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title(station.name)
+    plt.axhline(v, color= 'red', linestyle='--', label='low value')
+    plt.axhline(h, color='blue', linestyle='--', label='high value')
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    plt.show()
