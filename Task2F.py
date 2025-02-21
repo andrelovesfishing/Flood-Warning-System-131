988e8a1a1917c8f6e69ef8670282ba6a5c33b8f7
andrelovesfishing <am3331@cam.ac.uk>
2025-02-21 17:31:40 +0000

    Improved task 2f to include top 5 stations

--- Task2F.py
+++ Task2F.py
@@ -7,33 +7,40 @@ import datetime
 def run():
     # Build list of stations
     stations = build_station_list()
-    
-    station_name = "Addlestone"
 
-    # Find station
-    station_cam = None
-    for station in stations:
-        if station.name == station_name:
-            station_cam = station
-            break
-    
-    # Check that station could be found. Return if not found.
-    if not station_cam:
-        print("Station {} could not be found".format(station_name))
-        return
+    top_five_stations = []
+    stations_list = stations_highest_rel_level(stations,5)
+    for item in stations_list:
+        top_five_stations.append(item[0])
 
-    # Fetch and plot data for the past 10 days for each of these stations
-    dt = 10
-    dates, levels = fetch_measure_levels(
-        station_cam.measure_id, dt=datetime.timedelta(days=dt))
-    list_of_dates = []
-    list_of_levels = []
-    for date, level in zip(dates, levels):
-        list_of_dates.append(date.replace(tzinfo=None))
-        list_of_levels.append(level)
-    
-    # Print level history
-    plot_water_level_with_fit(station_cam, list_of_dates, list_of_levels,2)
+    # Station name to find
+    for station in top_five_stations:
+        station_name = station
+
+        # Find station
+        station_cam = None
+        for station in stations:
+            if station.name == station_name:
+                station_cam = station
+                break
+
+        # Check that station could be found. Return if not found.
+        if not station_cam:
+            print("Station {} could not be found".format(station_name))
+            return
+
+        # Fetch and plot data for the past 10 days for each of these stations
+        dt = 10
+        dates, levels = fetch_measure_levels(
+            station_cam.measure_id, dt=datetime.timedelta(days=dt))
+        list_of_dates = []
+        list_of_levels = []
+        for date, level in zip(dates, levels):
+            list_of_dates.append(date.replace(tzinfo=None))
+            list_of_levels.append(level)
+
+        # Print level history
+        plot_water_level_with_fit(station_cam, list_of_dates, list_of_levels,2)
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -11,9 +11,10 @@ def plot_water_levels(station, dates, levels):
 
     low = station.typical_range[0]
     high = station.typical_range[1]
-    
+
     if len(dates) != len(levels):
         raise ValueError("Length of dates and levels must be the same")
+    
     # Plot
     plt.plot(dates, levels)
 
@@ -31,11 +32,17 @@ def plot_water_levels(station, dates, levels):
     plt.show()
 
 def plot_water_level_with_fit(station, dates, levels, p):
+    
+    low = station.typical_range[0]
+    high = station.typical_range[1]
+
+    if len(dates) != len(levels):
+        raise ValueError("Length of dates and levels must be the same")
+
     poly_coeff, d0 = polyfit(dates,levels,p)
     true_dates = mdates.date2num(dates)
-    print(poly_coeff, "- poly")
     polynomial_expression = np.poly1d(poly_coeff)
-    print(polynomial_expression, "- y")
+
     plt.plot(dates, levels)
     plt.plot(dates,polynomial_expression(true_dates))
     
@@ -45,5 +52,8 @@ def plot_water_level_with_fit(station, dates, levels, p):
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45)
     plt.title(station.name)
+    plt.axhline(low, color= 'red', linestyle='--', label='low value')
+    plt.axhline(high, color='blue', linestyle='--', label='high value')
+
 
     plt.show()
